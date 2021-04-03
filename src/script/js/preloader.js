08209a54d5f39cfec760fa954fbe7631673b070b
Andrii Chornyi <andchorniy@gmail.com>
2021-04-03 10:13:51 +0200

    rename scss component

--- src/script/js/preloader.js
+++ src/script/js/preloader.js
@@ -1,7 +1,5 @@
 import refs from './refs';
 window.addEventListener('load', () => {
-  console.log('loaded');
-  // refs.preload.classList.add('hide-preloader');
   setTimeout(() => {
     refs.preload.style.visibility = 'hidden';
     refs.preload.style.opacity = '0';
