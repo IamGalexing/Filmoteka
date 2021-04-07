9b69849dc3bf9f82400daea179acceffc39adfa7
Yevgeniya Alexeyeva <y.alekseyeva@gmail.com>
2021-04-07 22:14:27 +0600

    removed listeners

--- src/script/js/search.js
+++ src/script/js/search.js
@@ -5,6 +5,16 @@ import FetchGenre from '../API/fetchGenre';
 import Pagination from './pagination-api';
 import createMarkup from '../templates/galleryCard.hbs';
 import _debounce from 'lodash.debounce';
+import {
+  showPrevPopPage,
+  showNextPopPage,
+  showSelectedPopPage,
+} from './popular-gallery';
+import {
+  showPrevFilterPage,
+  showNextFilterPage,
+  showSelectedFilterPage,
+} from './movieFilter';
 
 const {
   searchInputRef,
@@ -39,40 +49,7 @@ function hendlerInput(e) {
     return;
   }
   searchWrap.classList.add('without-after-el');
-  function createCard() {
-    // spinner.classList.remove('is-hidden');
-    apiSearchData
-      .fetchMovies()
-      .then(res => {
-        scrollWin();
-        if (res === []) return;
-        gallery.innerHTML = createMarkup(transformMovieObject(res.results));
-        // pagination
-        if (res.total_results > 20) {
-          noResultRef.textContent = '';
-          paginationContainer.classList.remove('visually-hidden');
-          paginationPrevButton.classList.remove('hidden');
-          paginationNextButton.classList.remove('hidden');
-          paginationPrevButton.addEventListener('click', showPrevPage);
-          paginationNextButton.addEventListener('click', showNextPage);
-          paginationWrapper.addEventListener('click', showSelectedPage);
-          paginationWrapper.innerHTML = pagination.renderPaginationMarkup(
-            apiSearchData.page,
-            res.total_results,
-          );
-        } else {
-          paginationWrapper.innerHTML = null;
-          paginationPrevButton.classList.add('hidden');
-          paginationNextButton.classList.add('hidden');
-        }
-        if (res.total_results === 0 || res.total_results === '') {
-          noResults();
-          // paginationContainer.classList.add('visually-hidden');
-        }
-      })
-      .catch(e => console.log(e))
-      .finally(spinner.classList.add('is-hidden'));
-  }
+ 
 
   function noResults() {
     noResultRef.textContent =
@@ -81,51 +58,100 @@ function hendlerInput(e) {
       noResultRef.textContent = '';
     }, 2000);
   }
-
+ 
   apiSearchData.resetPage();
   createCard();
 
-  function transformMovieObject(movies) {
-    movies.forEach(elem => {
-      if (elem.title.length > 38) {
-        elem.title = elem.title.slice(0, 38) + '...';
+ 
+}
+function createCard() {
+  // spinner.classList.remove('is-hidden');
+  apiSearchData
+    .fetchMovies()
+    .then(res => {
+      scrollWin();
+      if (res === []) return;
+      gallery.innerHTML = createMarkup(transformMovieObject(res.results));
+      // pagination
+
+      if (res.total_pages > 1) {
+        noResultRef.textContent = '';
+        paginationContainer.classList.remove('visually-hidden');
+        paginationPrevButton.classList.remove('hidden');
+        paginationNextButton.classList.remove('hidden');
+
+        paginationPrevButton.removeEventListener('click', showPrevPopPage);
+        paginationNextButton.removeEventListener('click', showNextPopPage);
+        paginationWrapper.removeEventListener('click', showSelectedPopPage);
+
+        paginationPrevButton.removeEventListener('click', showPrevFilterPage);
+        paginationNextButton.removeEventListener('click', showNextFilterPage);
+        paginationWrapper.removeEventListener(
+          'click',
+          showSelectedFilterPage,
+        );
+
+        paginationPrevButton.addEventListener('click', showPrevSearchPage);
+        paginationNextButton.addEventListener('click', showNextSearchPage);
+        paginationWrapper.addEventListener('click', showSelectedSearchPage);
+        paginationWrapper.innerHTML = pagination.renderPaginationMarkup(
+          apiSearchData.page,
+          res.total_results,
+        );
+      } else {
+        paginationWrapper.innerHTML = null;
+        paginationPrevButton.classList.add('hidden');
+        paginationNextButton.classList.add('hidden');
       }
-      elem.poster_path
-        ? (elem.poster_path = `https://image.tmdb.org/t/p/w500/${elem.poster_path}`)
-        : (elem.poster_path = reservImg);
-      elem.release_date
-        ? (elem.release_date = elem.release_date.slice(0, 4))
-        : (elem.release_date = 'Unknown');
-      elem.genre_ids
-        ? (elem.genre_ids = apiGenreData
-            .ganreTranspiler(elem.genre_ids)
-            .slice(0, 3)
-            .join(', '))
-        : (elem.genre_ids = 'Unknown');
-    });
-    return movies;
-  }
-  const showPrevPage = () => {
-    if (apiSearchData.page < 2) return;
-    apiSearchData.decrementPage();
-    createCard();
-  };
-  const showNextPage = () => {
-    const activePageNumber = document.querySelector('li.active');
-    if (apiSearchData.page === activePageNumber.textContent) return;
-    apiSearchData.incrementPage();
-    createCard();
-  };
-  const showSelectedPage = e => {
-    if (e.target.nodeName === 'LI') {
-      if (isNaN(e.target.textContent)) return;
-      apiSearchData.page = e.target.textContent;
-      createCard();
+      if (res.total_results === 0 || res.total_results === '') {
+        noResults();
+        paginationContainer.classList.add('visually-hidden');
+      }
+    })
+    .catch(e => console.log(e))
+    .finally(spinner.classList.add('is-hidden'));
+}
+function transformMovieObject(movies) {
+  movies.forEach(elem => {
+    if (elem.title.length > 38) {
+      elem.title = elem.title.slice(0, 38) + '...';
     }
-  };
-  function scrollWin() {
-    window.scrollTo(0, 0);
+    elem.poster_path
+      ? (elem.poster_path = `https://image.tmdb.org/t/p/w500/${elem.poster_path}`)
+      : (elem.poster_path = reservImg);
+    elem.release_date
+      ? (elem.release_date = elem.release_date.slice(0, 4))
+      : (elem.release_date = 'Unknown');
+    elem.genre_ids
+      ? (elem.genre_ids = apiGenreData
+          .ganreTranspiler(elem.genre_ids)
+          .slice(0, 3)
+          .join(', '))
+      : (elem.genre_ids = 'Unknown');
+  });
+  return movies;
+}
+const showPrevSearchPage = () => {
+  if (apiSearchData.page < 2) return;
+  apiSearchData.decrementPage();
+  createCard();
+};
+const showNextSearchPage = () => {
+  const activePageNumber = document.querySelector('li.active');
+  if (apiSearchData.page === activePageNumber.textContent) return;
+  apiSearchData.incrementPage();
+  createCard();
+};
+const showSelectedSearchPage = e => {
+  if (e.target.nodeName === 'LI') {
+    if (isNaN(e.target.textContent)) return;
+    apiSearchData.page = e.target.textContent;
+    createCard();
   }
+};
+function scrollWin() {
+  window.scrollTo(0, 0);
 }
 
 export default hendlerInput;
+export { showPrevSearchPage, showNextSearchPage, showSelectedSearchPage };
